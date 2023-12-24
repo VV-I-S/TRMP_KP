5f6a1f1ffa13e972a84b5faa7d393791154f22a3
VV-I-S <kborisova.priv@gmail.com>
2023-12-24 15:10:05 +0300

    user-profile

--- boosting-service/BoostingService/pages/Profile/UserProfile/UserProfile.style.ts
+++ boosting-service/BoostingService/pages/Profile/UserProfile/UserProfile.style.ts
@@ -7,10 +7,9 @@ const Styles = StyleSheet.create({
         height: 100,
     },
     container2: {
-        flex: 1,
         backgroundColor: '#E5D4FF',
         alignItems: 'center',
-        height: 100,
+        height: 200,
     },
     userName: {
         color: 'black',
@@ -19,6 +18,49 @@ const Styles = StyleSheet.create({
     userData: {
         color: 'black',
         fontSize: 20,
-    }
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    buttonOpen: {
+        backgroundColor: '#D0A2F7',
+    },
+    buttonClose: {
+        marginTop: 20,
+        backgroundColor: '#D0A2F7',
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+        fontSize: 20,
+    },
 });
 export default Styles
--- boosting-service/BoostingService/pages/Profile/UserProfile/UserProfile.tsx
+++ boosting-service/BoostingService/pages/Profile/UserProfile/UserProfile.tsx
@@ -1,8 +1,9 @@
-import React, {useEffect} from 'react'
-import {Text, View} from 'react-native'
+import React, {useEffect, useState} from 'react'
+import {Text, View, Button, Modal, Pressable, ScrollView, Alert} from 'react-native'
 import axios from 'axios'
 import { Avatar } from "react-native-paper";
 import Styles from './UserProfile.style'
+import { Table, TableWrapper, Row, Rows, Col } from 'react-native-table-component';
 
 export type OrderWaitingTypes = {
     id: number
@@ -27,11 +28,31 @@ const UserProfile = () => {
         orders: [],
         phone: '',
     })
+    const state = {
+        tableHead: ["ID", "Дата оформления", "Стоимость"],
+        widthArr: [100, 150, 150],
+    };
+    const generateTableData = (rowCount: number, columnCount: number) => {
+        const data = [];
 
+        for (let i = 1; i <= rowCount; i++) {
+            const row = [];
+            for (let j = 1; j <= columnCount; j++) {
+                row.push(`R${i} C${j}`);
+            }
+            data.push(row);
+        }
+
+        return data;
+    };
+
+    const data = generateTableData(5, 3);
     useEffect(() => {
         //TODO: как-то странно это все работает, не сразу записывается
         axios.get('/user/getUserInfo').then(({data}) => setUser(data))
     }, [])
+
+    const [modalVisible, setModalVisible] = useState(false);
 
     return (
         <View style={Styles.container}>
@@ -43,9 +64,60 @@ const UserProfile = () => {
             </View>
             <View>
                 <Text style={Styles.userName}>Текущий заказ</Text>
+                <Text>Начальный ММР:</Text>
+                <Text>Конечный ММР: </Text>
+                <Text>Количество игр SD: </Text>
+                <Text>Стоимость:  руб.</Text>
+                <Text>Статус</Text>
+                <Button title="В зависимости от статуса"></Button>
             </View>
             <View>
                 <Text style={Styles.userName}>История заказов</Text>
+                <Table borderStyle={{ borderColor: "#C1C0B9" }}>
+                    <Row
+                        data={state.tableHead}
+                        widthArr={state.widthArr}
+                    />
+                </Table>
+                <ScrollView>
+                    <Table borderStyle={{ borderColor: "#C1C0B9" }}>
+                        {data.map((dataRow, index) => (
+                            <Row
+                                key={index}
+                                data={dataRow}
+                                widthArr={state.widthArr}
+                            />
+                        ))}
+                    </Table>
+                </ScrollView>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert('Modal has been closed.');
+                        setModalVisible(!modalVisible);
+                    }}>
+                    <View style={Styles.centeredView}>
+                        <View style={Styles.modalView}>
+                            <Text style={Styles.modalText} >Начальный ММР:</Text>
+                            <Text style={Styles.modalText}>Конечный ММР: </Text>
+                            <Text style={Styles.modalText}>Количество игр SD: </Text>
+                            <Text style={Styles.modalText}>Стоимость:  руб.</Text>
+                            <Text style={Styles.modalText}>Статус</Text>
+                            <Pressable
+                                style={[Styles.button, Styles.buttonClose]}
+                                onPress={() => setModalVisible(!modalVisible)}>
+                                <Text  style={Styles.textStyle}>Hide Modal</Text>
+                            </Pressable>
+                        </View>
+                    </View>
+                </Modal>
+                <Pressable
+                    style={[Styles.button, Styles.buttonOpen]}
+                    onPress={() => setModalVisible(true)}>
+                    <Text style={Styles.textStyle}>Show Modal</Text>
+                </Pressable>
             </View>
         </View>
     )
