e9ffd287f18d7681f8f2b278b4452c3f3282866c
VV-I-S <kborisova.priv@gmail.com>
2023-12-24 21:23:28 +0300

    add-calibration

--- boosting-service/BoostingService/pages/Service/Calibration.tsx
+++ boosting-service/BoostingService/pages/Service/Calibration.tsx
@@ -1,4 +1,29 @@
 import React from 'react'
+import {Text, View, TextInput, Button, Pressable} from 'react-native'
+import {Slider} from 'react-native-elements'
+import Styles from './Service.style'
 
-const Calibration = () => {}
+const Calibration = () => {
+  return (
+    <View style={Styles.container}>
+      <View style={Styles.container3}>
+        <Text style={Styles.formLabel}>Оформить заказ</Text>
+        <TextInput
+          style={Styles.inputStyle2}
+          placeholder="Текущий ммр"></TextInput>
+        <View></View>
+        <View style={Styles.formText}>
+          <Text style={Styles.formText}>Конечный ммр:</Text>
+          <Text style={Styles.formText}>Стоимость (со скидкой)</Text>
+          <Text style={Styles.formText}>Стоимость</Text>
+          <Text style={Styles.formText}>Дней</Text>
+        </View>
+
+        <Pressable style={Styles.button}>
+          <Text style={Styles.textStyle}>Оформить заказ</Text>
+        </Pressable>
+      </View>
+    </View>
+  )
+}
 export default Calibration
